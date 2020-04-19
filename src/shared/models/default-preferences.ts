3da1b1c22f0ad5fe5e2b7f9f85547b7f7b7f74b0
EnderDev <xenderpugx@gmail.com>
2020-04-19 21:51:45 +0100

    🚀 Add privacy category to preferences

--- src/shared/models/default-preferences.ts
+++ src/shared/models/default-preferences.ts
@@ -1,17 +1,41 @@
 export interface DEFAULT_PREFERENCES {
     appearance: {
-        theme: "light" | "dark" | "oled"
+        theme: "auto" | "light" | "dark" | "oled",
+        showHomeButton: boolean
     },
     search: {
         provider: "duckduckgo" | "google" | "bing" | "yahoo" | "ecosia"
+    },
+    privacy: {
+        adblocker: boolean,
+        disableBatteryAPI: boolean,
+        block3rdPartyCookies: boolean,
+        doNotTrack: boolean,
+        maskSystemInformation: boolean,
+        maskColorDepth: boolean,
+        removeReferer: boolean,
+        customReferer: string,
+        hideIPAddress: boolean
     }
 }
 
 export const DEFAULT_PREFERENCES_OBJECT: DEFAULT_PREFERENCES = {
     appearance: {
-        theme: "light"
+        theme: "auto",
+        showHomeButton: false
     },
     search: {
         provider: "duckduckgo"
+    },
+    privacy: {
+        adblocker: true,
+        disableBatteryAPI: true,
+        block3rdPartyCookies: false,
+        doNotTrack: true,
+        maskSystemInformation: true,
+        maskColorDepth: true,
+        removeReferer: true,
+        customReferer: "Referrer removed by Dot",
+        hideIPAddress: true
     }
 }
