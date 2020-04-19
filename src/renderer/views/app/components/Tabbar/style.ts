b243e73aa8239665bc644e03c67bde7a4eb4fa0b
EnderDev <xenderpugx@gmail.com>
2020-04-19 21:57:03 +0100

    ⚡ Move Tabs to Tabbar

--- src/renderer/views/app/components/Tabbar/style.ts
+++ src/renderer/views/app/components/Tabbar/style.ts
@@ -10,7 +10,7 @@ export const StyledTabbar = styled.div`
   position: relative;
   overflow: hidden;
   transition: 0.3s opacity, 0.3s transform;
-  margin-left: 8px;
+  margin-left: 5px;
   margin-right: 32px;
   display: flex;
 `;
@@ -28,7 +28,8 @@ export const TabsContainer = styled.div`
 export const AddTab = styled(ToolbarButton)`
   position: absolute;
   left: 0;
-  top: 0;
+  top: 5px;
+  max-height: 32px;
 
   ${({ theme }: { theme?: ITheme }) => css`
     filter: ${theme['toolbar-addtab-filter']}
